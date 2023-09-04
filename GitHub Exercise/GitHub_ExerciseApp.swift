92503893f79924122797ab90a1e91eedfe38ed67
Dr. Thuerauf <alexander@thuer-auf.de>
2023-09-04 10:25:31 +0200

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Mac Thuerauf on 04.09.23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
